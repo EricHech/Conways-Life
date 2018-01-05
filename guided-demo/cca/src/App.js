22496e3d34fe5f0279f81105a7c339740efd4c8a
Brian "Beej Jorgensen" Hall <beej@beej.us>
2018-01-04 17:00:36 -0800

    added

---
+++ guided-demo/cca/src/App.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import CCA from './cca';
+import './App.css';
+
+const COLORS = [
+  [0, 0, 0],
+  [0x8f, 0, 0x5f],
+  [0x5f, 0, 0x8f],
+  [0, 0, 0xff],
+  [0, 0x5f, 0x7f],
+  [0x5f, 0x8f, 0x7f],
+  [0x8f, 0xff, 0x7f],
+  [0xff, 0x5f, 0x7f],
+]
+
+/**
+ * CCA canvas
+ */
+class CCACanvas extends Component {
+
+  /**
+   * Constructor
+   */
+  constructor(props) {
+    super(props);
+
+    this.cca = new CCA(props.width, props.height);
+    this.cca.randomize();
+  }
+
+  /**
+   * Component did mount
+   */
+  componentDidMount() {
+    requestAnimationFrame(() => {this.animFrame()});
+  }
+
+  /**
+   * Handle an animation frame
+   */
+  animFrame() {
+    let width = this.props.width;
+    let height = this.props.height;
+
+    // Update cca and get cells
+    let cells = this.cca.getCells();
+
+    // Get canvas framebuffer, a packed RGBA array
+    let canvas = this.refs.canvas;
+    let ctx = canvas.getContext('2d');
+    let imageData = ctx.getImageData(0, 0, width, height);
+
+    // Convert the cell values into white or black for the canvas
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+
+        // Index needs to be multiplied by 4 because there are 4 array
+        // entries per pixel, Red, Green, Blue, and Alpha:
+        let index = (y * width + x) * 4;
+
+        let ccaStatus = cells[y][x];
+
+        // FYI: Alpha channel controls how transparent a pixel is.
+
+        imageData.data[index + 0] = COLORS[ccaStatus][0]; // Red channel
+        imageData.data[index + 1] = COLORS[ccaStatus][1]; // Green channel
+        imageData.data[index + 2] = COLORS[ccaStatus][2]; // Blue channel
+        imageData.data[index + 3] = 0xff;  // Alpha channel, 0xff = opaque
+      }
+    }
+
+    // Put the new image data back on the canvas
+    ctx.putImageData(imageData, 0, 0);
+    
+    // Next generation of cca
+    this.cca.step();
+
+    // Request another animation frame
+    requestAnimationFrame(() => {this.animFrame()});
+  }
+
+  /**
+   * Render
+   */
+  render() {
+    return <canvas ref="canvas" width={this.props.width} height={this.props.height} />
+  }
+}
+
+/**
+ * CCA holder component
+ */
+class CCAApp extends Component {
+
+  /**
+   * Render
+   */
+  render() {
+    return (
+      <div>
+        <CCACanvas width={400} height={300} />
+      </div>
+    )
+  }
+}
+
+/**
+ * Outer App component
+ */
+class App extends Component {
+
+  /**
+   * Render
+   */
+  render() {
+    return (
+      <div className="App">
+        <CCAApp />
+      </div>
+    );
+  }
+}
+
+export default App;
